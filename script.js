7465fe65301054343322648e6e7f31279ed79e1b
Nnalue Fabian Izuchukwu <nnaluefabian@gmail.com>
2025-01-24 12:23:37 +0100

    Add fom element with html css and javascript

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("registration-form");
+  const feedbackDiv = document.getElementById("form-feedback");
+
+  form.addEventListener("submit", function (event) {
+    const userName = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const email = document.getElementById("email").value.trim();
+
+    const isValid = true;
+    const messages = [];
+
+    if (userName.length < 3) {
+      isValid = false;
+      messages.push("Username must be at least 3 characters long");
+    }
+
+    if (email.includes("@") && email.includes(".")) {
+      return;
+    } else {
+      isValid = false;
+      messages.push("Email must contain an '@' and a '.' sign");
+    }
+
+    if (password.length < 8) {
+      isValid = false;
+      messages.push("Password must be at least 8 characters long");
+    }
+    feedbackDiv.style.display = "block";
+    feedbackDiv.innerHTML = messages.join("<br>");
+  });
+});
